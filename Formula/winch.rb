2a001a73f6c853ac87136f0365e1f900ee1de8ae
Seth Yates <29878+sethyates@users.noreply.github.com>
2021-01-05 01:05:34 -0800

    chore: update

--- Formula/winch.rb
+++ Formula/winch.rb
@@ -1,11 +1,11 @@
 class Winch < Formula
   desc "Universal build and release tool"
   homepage "https://github.com/winchci/winch"
-  version "1.3.6"
+  version "1.4.7"
   bottle :unneeded
 
-  url "https://github.com/winchci/winch/releases/download/v1.3.6/darwin-amd64.tgz"
-  sha256 "570cbe2fa1833af293b08d0187eda11ae7839a9445b011c6bf433b258b6f8471"
+  url "https://github.com/winchci/winch/releases/download/v1.4.7/darwin-amd64.tgz"
+  sha256 "00ef92555429e2d5fa0ceb7dfbcb00c8e0eb0c7d9653b980598e9394009003a9"
 
   def install
     bin.install "winch"
