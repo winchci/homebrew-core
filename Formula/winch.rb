f2bf2f15d7e4755eaac2d7e7aeca3a963b4e355c
Seth Yates <29878+sethyates@users.noreply.github.com>
2021-05-02 00:21:11 -0700

    chore: upgrade

--- Formula/winch.rb
+++ Formula/winch.rb
@@ -1,23 +1,19 @@
 class Winch < Formula
   desc "Universal build and release tool"
   homepage "https://github.com/winchci/winch"
-  version "1.4.7"
+  version "1.8.5"
   bottle :unneeded
 
-  url "https://github.com/winchci/winch/releases/download/v1.4.7/darwin-amd64.tgz"
-  sha256 "00ef92555429e2d5fa0ceb7dfbcb00c8e0eb0c7d9653b980598e9394009003a9"
+  url "https://github.com/winchci/winch/releases/download/v1.8.5/darwin-amd64.tgz"
+  sha256 "c03e6fcda8d56e3ebd52da747ea29788cb589ff38aed18e40093e2bc3308b418"
 
   def install
     bin.install "winch"
-    bin.install "winch-go-errcheck"
-    bin.install "winch-go-imports"
-    bin.install "winch-go-lint"
-    bin.install "winch-go-sec"
-    bin.install "winch-go-shadow"
-    bin.install "winch-go-staticcheck"
+
   end
 
   test do
     system "#{bin}/winch --version"
+
   end
 end
